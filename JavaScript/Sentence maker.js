74952e04b794a6802f91361c2f35e9bc055ff4c3
Snow_flakes <ntsakomaluleke474@gmail.com>
2025-10-13 11:59:13 +0200

    JavaScript projects

---
+++ JavaScript/Sentence maker.js
@@ -0,0 +1,18 @@
+let adjective = "clever";
+let noun = "student";
+let verb = "open";
+let place = "home";
+let adjective2 = "small";
+let noun2 = "apples";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " +  firstStory);
+
+adjective = "small";
+noun = "rabbit";
+verb = "ignited";
+place = "hole";
+adjective2 = "big";
+noun2 = "carrots";
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log("Second story: " + secondStory);
---
+++ JavaScript/Trivia Bot.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "John";
+
+let botLocation = "Cape Town";
+let favoriteLanguage = "javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know that " + favoriteLanguage + " is the third most popular language" + ".";
+console.log(codingFact);
+
+codingFact = "It is recommended that one should learn " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was created in 1995" + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
